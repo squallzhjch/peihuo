2283d85a1f47995936e671784cb063da83f32612
squallzhjch <zhangjingchao@navinfon.com>
2017-08-18 23:25:10 +0800

    2

---
+++ PeiHuoApp/src/main/java/com/peihuo/system/SystemConfig.java
@@ -0,0 +1,11 @@
+package com.peihuo.system;
+
+import android.os.Environment;
+
+/**
+ * Created by 123 on 2017/8/18.
+ */
+
+public class SystemConfig {
+    public static final String SD_PATH = Environment.getExternalStorageDirectory().toString();
+}
